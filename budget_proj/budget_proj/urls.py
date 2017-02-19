c121bdc98686d2b6e99c5c2113ee319c04ef5b5b
Jim Tyhurst <jim@tyhurst.com>
2017-02-19 12:48:23 -0800

    Changed reference from non-existent example_app to budget_proj.

--- budget_proj/budget_proj/urls.py
+++ budget_proj/budget_proj/urls.py
@@ -19,5 +19,5 @@ from django.conf.urls import include
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^', include('example_app.urls', namespace='budget_app')),
+    url(r'^', include('budget_app.urls', namespace='budget_app')),
 ]
