52350afd721efb303837fb75346ced62dd6a5874
Hassan Shamim <hassan.has.you@gmail.com>
2017-02-21 14:12:52 -0800

    fix fragile csv finding in KPM/OCRB viewss

--- budget_proj/budget_proj/settings.py
+++ budget_proj/budget_proj/settings.py
@@ -15,6 +15,9 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+# Set directory that contains our csv data etc.
+BASE_DATA_DIR = os.path.join(os.path.dirname(BASE_DIR), 'Data')
+
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.10/howto/deployment/checklist/
@@ -38,7 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     # *******************************
-    # added outside of default setup 
+    # added outside of default setup
     'rest_framework',
     'budget_app',
     # *******************************
