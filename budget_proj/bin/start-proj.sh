83be69c8819632ec07fcab332f0728ad78ec87dd
Mike Lonergan <mikethecanuck@gmail.com>
2017-03-07 15:08:18 -0800

    cleanup in aisle 5

--- budget_proj/bin/start-proj.sh
+++ budget_proj/bin/start-proj.sh
@@ -1,7 +1,4 @@
 #! /bin/bash
 source ../bin/env.sh
 
-# doing this here only for local builds, shouldn't be needed for Travis
-sudo chmod +x ../docker-entrypoint.sh
-sudo chmod +x ../bin/start-proj.sh
 docker-compose up
