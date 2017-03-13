5ee273b0375b99906536ba039b0a9c2c02272a56
Jim Tyhurst <jim@tyhurst.com>
2017-03-12 19:53:48 -0700

    Removed duplicate references.

--- budget_proj/budget_app/views.py
+++ budget_proj/budget_app/views.py
@@ -33,18 +33,17 @@ class ListOcrb(generics.ListCreateAPIView):
         Uses query parameters to select items to be returned from the database that summarizes Operating and Capital Requirements by Bureau.
         Note: Parameter names are case-insensitive, but parameter values are case-sensitive.
         """
-        serialized_data = None
         if(request.GET.keys()):
             # Build a dictionary of query parameters and their values.
             filter_dict = {}
             for key, value in request.GET.items():
                 filter_dict[key.lower()] = value  # Assumes all model attributes are lowercase.
-            # TODO: Make filtering be case-insensitive for parameter values .
+            # TODO: Make filtering be case-insensitive for parameter values.
             ocrbs = models.OCRB.objects.filter(**filter_dict)
         else:
             ocrbs = self.get_queryset()
         sorted_ocrbs = ocrbs.order_by('fy', 'budget_type', 'service_area', 'bureau', 'budget_category')
-        serialized_data = serializers.OcrbSerializer(sorted_ocrbs, many=True)
+        serialized_data = self.serializer_class(sorted_ocrbs, many=True)
         return Response(serialized_data.data)
 
 
