71e7728f758d0e5032d3a9722bbc4f10ba1a0fa2
mikethecanuck <mikethecanuck@gmail.com>
2017-03-12 23:29:02 -0700

    realized I recreated the up command and just needed to add kill after

--- budget_proj/bin/build-test-proj.sh
+++ budget_proj/bin/build-test-proj.sh
@@ -5,6 +5,10 @@
 # --- this resulted in "ERROR: No container found for budget-service_1"
 # - thus we're trying to run these all in sequence in a single script, hoping the just-built container is findable
 
-docker-compose -f budget_proj/docker-compose.yml build
-docker-compose -f budget_proj/docker-compose.yml start budget-service
+#docker-compose -f budget_proj/docker-compose.yml build
+#docker-compose -f budget_proj/docker-compose.yml start budget-service
+
+# Starting the container in background to enable us to run tests right after - only because "run python manage.py test" hits ImportError
+docker-compose -f budget_proj/docker-compose.yml up -d
 docker-compose -f budget_proj/docker-compose.yml exec budget-service python manage.py test
+docker-compose -f budget_proj/docker-compose.yml kill budget-service
