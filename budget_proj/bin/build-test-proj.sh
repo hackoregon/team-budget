19294d056ffae65c29eb8ba25882fb3026832750
mikethecanuck <mikethecanuck@gmail.com>
2017-03-13 09:11:17 -0700

    trying to get the container running in Travis

--- budget_proj/bin/build-test-proj.sh
+++ budget_proj/bin/build-test-proj.sh
@@ -5,6 +5,5 @@
 # --- this resulted in "ERROR: No container found for budget-service_1"
 # - thus we're trying to run these all in sequence in a single script, hoping the just-built container is findable
 
-docker-compose -f budget_proj/docker-compose.yml build budget-service
-docker-compose -f budget_proj/docker-compose.yml start budget-service
+docker-compose -f budget_proj/docker-compose.yml build budget-service && docker-compose -f budget_proj/docker-compose.yml start budget-service
 docker-compose -f budget_proj/docker-compose.yml exec budget-service python manage.py test
