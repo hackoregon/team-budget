0bc44f1cfa560288fdf00d23191961bb8fa6fc6a
mikethecanuck <mikethecanuck@gmail.com>
2017-03-15 10:54:41 -0700

    notes for future troubleshooting

--- budget_proj/budget_app/tests.py
+++ budget_proj/budget_app/tests.py
@@ -1,3 +1,8 @@
 from django.test import TestCase
 
 # Create your tests here.
+
+# Ideas:
+# - http://stackoverflow.com/questions/24904362/how-to-write-unit-tests-for-django-rest-framework-apis
+# - https://github.com/hackoregon/hacku-devops-2017/wiki/Assignment-5
+# - http://www.django-rest-framework.org/api-guide/testing/
