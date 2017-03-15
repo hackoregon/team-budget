9990b7034e93374fb008afd2aceb37dbad779196
mikethecanuck <mikethecanuck@gmail.com>
2017-03-14 17:11:01 -0700

    Django test script runs successfully in Docker when all env vars are hard-coded in Travis repo Settings

--- budget_proj/bin/test-proj.sh
+++ budget_proj/bin/test-proj.sh
@@ -1,12 +1,9 @@
 #! /bin/bash
 
-# PURPOSE: used to test that the DRF app is running inside the Docker container
+# PURPOSE: this script is used to test that the DRF app inside the Docker container is actually responding
 # Can be used in Travis build or on local developer machine
 
 echo  Running test_proj.sh...
-
-# Temporary troubleshooting
-echo DATABASE_PORT $DATABASE_PORT
 
 # Run all configured unit tests inside the Docker container
 docker-compose -f budget_proj/docker-compose.yml run budget-service python manage.py test
