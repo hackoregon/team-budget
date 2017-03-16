dfda382bbf9bc3b5e29852de807d24a77c3d584d
mikethecanuck <mikethecanuck@gmail.com>
2017-03-16 10:55:55 -0700

    added simplest health test for OCRB endpoint

--- budget_proj/budget_app/tests.py
+++ budget_proj/budget_app/tests.py
@@ -1,8 +1,20 @@
-from django.test import TestCase
-
-# Create your tests here.
+from django.test import TestCase, Client
+from django.urls import reverse
+from budget_app.models import OCRB
+from mixer.backend.django import mixer
+from rest_framework import status
+from rest_framework.test import APITestCase
 
 # Ideas:
 # - http://stackoverflow.com/questions/24904362/how-to-write-unit-tests-for-django-rest-framework-apis
 # - https://github.com/hackoregon/hacku-devops-2017/wiki/Assignment-5
 # - http://www.django-rest-framework.org/api-guide/testing/
+
+class TestOcrbEndpoint(TestCase):
+    def setup(self):
+        self.client = Client()
+
+    def test(self):
+        response = self.client.get('/ocrb/')
+
+        self.assertEqual(response.status_code, 200)
