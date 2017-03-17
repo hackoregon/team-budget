9a58c69eda24250193a658154edaf84b42378c7f
mikethecanuck <mikethecanuck@gmail.com>
2017-03-17 13:36:09 -0700

    naming the URLs to make automated testing easier

--- budget_proj/budget_app/urls.py
+++ budget_proj/budget_app/urls.py
@@ -7,8 +7,8 @@ schema_view = get_swagger_view(title='hackoregon_budget')
 # Listed in alphabetical order.
 urlpatterns = [
 	url(r'^$', schema_view),
-    url(r'^kpm/$', views.ListKpm.as_view()),
-    url(r'^ocrb/$', views.ListOcrb.as_view()),
-    url(r'^history/$', views.ListBudgetHistory.as_view()),
-    url(r'^code/$', views.ListLookupCode.as_view()),
+    url(r'^code/$', views.ListLookupCode.as_view(), name='code-list'),
+    url(r'^history/$', views.ListBudgetHistory.as_view(), name='history-list'),
+    url(r'^kpm/$', views.ListKpm.as_view(), name='kpm-list'),
+    url(r'^ocrb/$', views.ListOcrb.as_view(), name='ocrb-list'),
 ]
