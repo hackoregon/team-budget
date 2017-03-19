63e226211d18de0cef0c5c758e689e27d79e69e6
mikethecanuck <mikethecanuck@gmail.com>
2017-03-19 15:52:18 -0700

    adapting backend-service-pattern for automated testing

--- budget_proj/bin/docker-entrypoint.sh
+++ budget_proj/bin/docker-entrypoint.sh
@@ -2,5 +2,9 @@
 
 echo  Running docker-entrypoint.sh...
 
+export PATH=$PATH:~/.local/bin
+./budget_proj/bin/getconfig.sh
+python manage.py collectstatic --noinput
 # Fire up a lightweight frontend to host the DRF endpoints - gunicorn is convenient but other engines can be considered
-gunicorn budget_proj.wsgi:application -b :8000
+#gunicorn homelessAPI.wsgi:application -b :8000
+python manage.py test --no-input
