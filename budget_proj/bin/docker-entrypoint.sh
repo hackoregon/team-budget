e26bab29298cc84e6607da58a4c96872fdbfbe20
mikethecanuck <mikethecanuck@gmail.com>
2017-03-19 15:59:04 -0700

    always with the pathing issues

--- budget_proj/bin/docker-entrypoint.sh
+++ budget_proj/bin/docker-entrypoint.sh
@@ -3,8 +3,8 @@
 echo  Running docker-entrypoint.sh...
 
 export PATH=$PATH:~/.local/bin
-./budget_proj/bin/getconfig.sh
-python manage.py collectstatic --noinput
+./bin/getconfig.sh
+#python manage.py collectstatic --noinput
 # Fire up a lightweight frontend to host the DRF endpoints - gunicorn is convenient but other engines can be considered
 #gunicorn homelessAPI.wsgi:application -b :8000
 python manage.py test --no-input
