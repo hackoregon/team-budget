35cbc290ee007d028c51e1b7b068a55a4ca851ce
mikethecanuck <mikethecanuck@gmail.com>
2017-03-20 14:48:48 -0700

    stupid syntax or stupid engineer

--- budget_proj/bin/start-proj.sh
+++ budget_proj/bin/start-proj.sh
@@ -12,7 +12,7 @@ while getopts ":lt" opt; do
     case "$opt" in
         l)
           # This is an unfortunate workaround to the subdirectory that is used to contain all app code
-          export PROJ_SETTINGS_DIR = budget_proj
+          export PROJ_SETTINGS_DIR='budget_proj'
           docker-compose -f $PROJ_SETTINGS_DIR/local-docker-compose.yml up --build
           ;;
         t)
