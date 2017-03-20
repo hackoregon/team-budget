8c751a20012dfc0ae3472d22c284171909220e20
mikethecanuck <mikethecanuck@gmail.com>
2017-03-20 09:25:15 -0700

    adjusted scripts to ensure they work in offline local usage too

--- budget_proj/bin/docker-entrypoint.sh
+++ budget_proj/bin/docker-entrypoint.sh
@@ -6,5 +6,5 @@ export PATH=$PATH:~/.local/bin
 #./bin/getconfig.sh
 #python manage.py collectstatic --noinput
 # Fire up a lightweight frontend to host the DRF endpoints - gunicorn is convenient but other engines can be considered
-#gunicorn homelessAPI.wsgi:application -b :8000
-python manage.py test --no-input
+gunicorn budget_proj.wsgi:application -b :8000
+#python manage.py test --no-input
