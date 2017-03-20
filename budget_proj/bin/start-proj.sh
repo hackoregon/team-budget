7b162e17cb92d90bddce8413060df355d6017756
mikethecanuck <mikethecanuck@gmail.com>
2017-03-20 11:49:35 -0700

    enable getconfig.sh only where Travis needs it

--- budget_proj/bin/start-proj.sh
+++ budget_proj/bin/start-proj.sh
@@ -1,11 +1,23 @@
 #! /bin/bash
 
-# PURPOSE: used to launch the DRF app inside the Docker container
+usage() { echo "Usage: $0 [-l] for a local build or [-t] for a travis build " 1>&2; exit 1; }
+
+# PURPOSE: used to launch the Django app inside the Docker container
 # Can be used on local developer machine; if used in Travis build, will fail the build after 10min timeout
 
 echo  Running start-proj.sh...
 
-export DEPLOY_TARGET='local'
-
 # Builds and launches the Docker container to be run in daemon mode - requires [CTRL]-C to stop the container
-docker-compose -f budget_proj/docker-compose.yml up --build
+while getopts ":lt" opt; do
+    case "$opt" in
+        l)
+          docker-compose -f $PROJ_SETTINGS_DIR/local-docker-compose.yml up --build
+          ;;
+        t)
+          docker-compose -f $PROJ_SETTINGS_DIR/travis-docker-compose.yml up
+          ;;
+        *)
+          usage
+          ;;
+    esac
+done
