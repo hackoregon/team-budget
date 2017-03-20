bd013394fbde7d80c8f8dc7d45a0eca2f8ba8a9c
mikethecanuck <mikethecanuck@gmail.com>
2017-03-19 17:45:26 -0700

    removing redundant getconfig script in docker-entrypoint

--- budget_proj/bin/docker-entrypoint.sh
+++ budget_proj/bin/docker-entrypoint.sh
@@ -3,7 +3,7 @@
 echo  Running docker-entrypoint.sh...
 
 export PATH=$PATH:~/.local/bin
-./bin/getconfig.sh
+#./bin/getconfig.sh
 #python manage.py collectstatic --noinput
 # Fire up a lightweight frontend to host the DRF endpoints - gunicorn is convenient but other engines can be considered
 #gunicorn homelessAPI.wsgi:application -b :8000
