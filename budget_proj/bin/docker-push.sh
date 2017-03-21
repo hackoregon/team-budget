82ea29809545bf165a21d290183b78494ebf3b20
mikethecanuck <mikethecanuck@gmail.com>
2017-03-21 14:59:21 -0700

    adding the damned excess path to this script call too

--- budget_proj/bin/docker-push.sh
+++ budget_proj/bin/docker-push.sh
@@ -7,7 +7,7 @@ if [ -z "$TRAVIS_PULL_REQUEST" ] || [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
     export PATH=$PATH:$HOME/.local/bin
     docker login -u "$DOCKER_USERNAME" -p "$DOCKER_PASSWORD" "$DOCKER_REPO"
     docker push "$DOCKER_REPO"/"$DEPLOY_TARGET"/"$DOCKER_IMAGE":latest
-    ./bin/ecs-deploy.sh  \
+    ./$PROJ_SETTINGS_DIR/bin/ecs-deploy.sh  \
      -n "$ECS_SERVICE_NAME" \
      -c "$ECS_CLUSTER"   \
      -i "$DOCKER_REPO"/"$DEPLOY_TARGET"/"$DOCKER_IMAGE":latest
