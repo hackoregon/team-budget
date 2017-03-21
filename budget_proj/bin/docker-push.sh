01d903a2a46b847b6f3fb7d4ce17e9a35274f6e1
mikethecanuck <mikethecanuck@gmail.com>
2017-03-21 14:19:49 -0700

    temporarily enabling the push from all branches to test whether push works

--- budget_proj/bin/docker-push.sh
+++ budget_proj/bin/docker-push.sh
@@ -3,7 +3,7 @@
 # Tag, Push and Deploy only if it's not a pull request
 if [ -z "$TRAVIS_PULL_REQUEST" ] || [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
   # Push only if we're testing the master branch
-  if [ "$TRAVIS_BRANCH" == "master" ]; then
+#  if [ "$TRAVIS_BRANCH" == "master" ]; then
     pip install --user awscli
     export PATH=$PATH:$HOME/.local/bin
     docker login -u "$DOCKER_USERNAME" -p "$DOCKER_PASSWORD" "$DOCKER_REPO"
@@ -12,9 +12,9 @@ if [ -z "$TRAVIS_PULL_REQUEST" ] || [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
      -n "$ECS_SERVICE_NAME" \
      -c "$ECS_CLUSTER"   \
      -i "$DOCKER_REPO"/"$DEPLOY_TARGET"/"$DOCKER_IMAGE":latest
-   else
-     echo "Skipping deploy because branch is not master"
-  fi
+#   else
+#     echo "Skipping deploy because branch is not master"
+#  fi
 else
   echo "Skipping deploy because it's a pull request"
 fi
