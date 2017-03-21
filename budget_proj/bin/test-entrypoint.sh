68010b0ea7037c7d039f390ed2112b8692104528
mikethecanuck <mikethecanuck@gmail.com>
2017-03-21 11:31:02 -0700

    switch to installing awscli locally using --user parameter and annotating all places where getconfig is called - since that script calls the aws CLI command

--- budget_proj/bin/test-entrypoint.sh
+++ budget_proj/bin/test-entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-export PATH=$PATH:~/.local/bin
+export PATH=$PATH:~/.local/bin # necessary to help locate the awscli binaries which are pip installed --user
 ./bin/getconfig.sh
 #python manage.py migrate --noinput
 python manage.py test --no-input
