b39719c621c0b2a4a89eb5535d4538378b1f160b
mikethecanuck <mikethecanuck@gmail.com>
2017-03-23 13:38:56 -0700

    cleanup the imports

--- budget_proj/budget_app/tests.py
+++ budget_proj/budget_app/tests.py
@@ -1,12 +1,13 @@
-from django.test import TestCase, Client
-from django.urls import reverse
-from budget_app.models import OCRB
-from mixer.backend.django import mixer
-from rest_framework import status
-from rest_framework.test import APITestCase
 import json # enables inspecting JSON data for certain fields
+from django.test import TestCase, Client
+# NOTE: disabled these for now but expect they'll be used in the near future
+# from django.urls import reverse
+# from budget_app.models import OCRB
+# from mixer.backend.django import mixer
+# from rest_framework import status
+# from rest_framework.test import APITestCase
 
-# Ideas:
+# Other Ideas:
 # - http://stackoverflow.com/questions/24904362/how-to-write-unit-tests-for-django-rest-framework-apis
 # - https://github.com/hackoregon/hacku-devops-2017/wiki/Assignment-5
 # - http://www.django-rest-framework.org/api-guide/testing/
