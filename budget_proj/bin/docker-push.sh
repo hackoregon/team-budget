9b651679a830fb41fa4a3afdcfdafdcb68d6b7c9
mikethecanuck <mikethecanuck@gmail.com>
2017-04-02 16:40:09 -0700

    trying some tricks that Brian Grant used in ER

--- budget_proj/bin/docker-push.sh
+++ budget_proj/bin/docker-push.sh
@@ -13,7 +13,9 @@ if [ -z "$TRAVIS_PULL_REQUEST" ] || [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
     ./$PROJ_SETTINGS_DIR/bin/ecs-deploy.sh  \
      -n "$ECS_SERVICE_NAME" \
      -c "$ECS_CLUSTER"   \
-     -i "$DOCKER_REPO"/"$DEPLOY_TARGET"/"$DOCKER_IMAGE":latest
+     -i "$DOCKER_REPO"/"$DEPLOY_TARGET"/"$DOCKER_IMAGE":latest \
+     -t 300 \ # Troubleshooting
+     -v # Troubleshooting
    else
      echo "Skipping deploy because branch is not master"
   fi
