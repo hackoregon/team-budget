19cfa8146a7f49f193e1a86a0519d50e6ad28443
mikethecanuck <mikethecanuck@gmail.com>
2017-04-08 14:48:11 -0700

    adding stderr of all gunicorn output to find out what is really going on with ALB requests

--- budget_proj/bin/docker-entrypoint.sh
+++ budget_proj/bin/docker-entrypoint.sh
@@ -7,7 +7,7 @@ echo  Running docker-entrypoint.sh...
 python manage.py collectstatic --no-input
 
 # Fire up a lightweight frontend to host the Django endpoints - gunicorn was the default choice
-gunicorn budget_proj.wsgi:application -b :8000 --keep-alive 60 --workers 5 --worker-class 'gevent' # gevent used to address ELB/gunicorn issue here https://github.com/benoitc/gunicorn/issues/1194
+gunicorn budget_proj.wsgi:application -b :8000 --keep-alive 60 --workers 5 --worker-class 'gevent' --log-file - # gevent used to address ELB/gunicorn issue here https://github.com/benoitc/gunicorn/issues/1194
 # other options to try to get past initial ALB health check when deploying a new container image:
 # --workers 3 (enough for the # of cores)
 # --timeout 90
