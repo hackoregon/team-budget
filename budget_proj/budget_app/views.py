f89c53c49f3c8997351fd3b05653785d1c8894ec
Hassan Shamim <hassan.has.you@gmail.com>
2017-04-08 13:45:27 -0700

    hack pagination into ocrb view

--- budget_proj/budget_app/views.py
+++ budget_proj/budget_app/views.py
@@ -39,13 +39,29 @@ class ListOcrb(generics.ListAPIView):
             # Build a dictionary of query parameters and their values.
             filter_dict = {}
             for key, value in request.GET.items():
-                filter_dict[key.lower() + "__iexact"] = value  # Assumes all model attributes are lowercase.
+                if key != 'page':
+                    filter_dict[key.lower() + "__iexact"] = value  # Assumes all model attributes are lowercase.
             ocrbs = models.OCRB.objects.filter(**filter_dict)
         else:
             ocrbs = self.get_queryset()
         sorted_ocrbs = ocrbs.order_by('fy', 'budget_type', 'service_area', 'bureau', 'budget_category')
+        page = self.paginate_queryset(sorted_ocrbs)
+        if page is not None:
+            serializer = self.get_serializer(page, many=True)
+            return self.get_paginated_response(serializer.data)
+
         serialized_data = self.serializer_class(sorted_ocrbs, many=True)
         return Response(serialized_data.data)
+    #   queryset = self.filter_queryset(self.get_queryset())
+
+        # page = self.paginate_queryset(queryset)
+        # if page is not None:
+        #     serializer = self.get_serializer(page, many=True)
+        #     return self.get_paginated_response(serializer.data)
+
+        # serializer = self.get_serializer(queryset, many=True)
+        # return Response(serializer.data)
+
 
 
 class OcrbSummary(generics.ListAPIView):
--- budget_proj/budget_proj/settings/base.py
+++ budget_proj/budget_proj/settings/base.py
@@ -73,6 +73,13 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'budget_proj.wsgi.application'
 
+
+# DRF Settings:
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 100
+}
+
 # Password validation
 # https://docs.djangoproject.com/en/1.10/ref/settings/#auth-password-validators
 
