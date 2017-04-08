a2eea522594752d05ea4bfe1d2bc9abdf7d80a7b
mikethecanuck <mikethecanuck@gmail.com>
2017-04-08 14:35:10 -0700

    trying all the gunicorn settings we think might help get past the ALB health check to get new containers actually serving traffic - and I am forcing deploy because the last deploy died anyway

--- budget_proj/bin/docker-entrypoint.sh
+++ budget_proj/bin/docker-entrypoint.sh
@@ -7,4 +7,8 @@ echo  Running docker-entrypoint.sh...
 python manage.py collectstatic --no-input
 
 # Fire up a lightweight frontend to host the Django endpoints - gunicorn was the default choice
-gunicorn budget_proj.wsgi:application -b :8000 --keep-alive 60 --worker-class 'gevent' # gevent used to address ELB/gunicorn issue here https://github.com/benoitc/gunicorn/issues/1194
+gunicorn budget_proj.wsgi:application -b :8000 --keep-alive 60 --workers 5 --worker-class 'gevent' # gevent used to address ELB/gunicorn issue here https://github.com/benoitc/gunicorn/issues/1194
+# other options to try to get past initial ALB health check when deploying a new container image:
+# --workers 3 (enough for the # of cores)
+# --timeout 90
+# vary the keep-alive (up or down?)
