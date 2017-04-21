039eef750183581995c8189795ecc09b5961a6f5
Moss Drake <mxmossman@gmail.com>
2017-04-21 15:35:48 -0700

    Update View-HistorySummaryByServiceArea.sql
    calculate the service area based on bureau code. #135

--- sql/View-HistorySummaryByServiceArea.sql
+++ sql/View-HistorySummaryByServiceArea.sql
@@ -4,12 +4,17 @@
 -- DROP VIEW public."HistorySummaryByServiceArea";
 
 CREATE OR REPLACE VIEW public."HistorySummaryByServiceArea" WITH (security_barrier=true) AS 
- SELECT budget_app_budgethistory.fiscal_year,
-    budget_app_budgethistory.service_area_code,
+SELECT 
+    budget_app_budgethistory.fiscal_year,
+    case 
+      when budget_app_budgethistory.bureau_code = 'MF' then 'LA'
+      when budget_app_budgethistory.bureau_code in ('MY','PA','PS','PW','PU','AU') then 'EO'
+      else budget_app_budgethistory.service_area_code
+    end as service_area_code,
     sum(COALESCE(budget_app_budgethistory.amount, 0)) AS sum
-   FROM budget_app_budgethistory
-  GROUP BY budget_app_budgethistory.fiscal_year, budget_app_budgethistory.service_area_code
-  ORDER BY budget_app_budgethistory.fiscal_year, budget_app_budgethistory.service_area_code;
-
+FROM budget_app_budgethistory
+  GROUP BY 1,2
+  ORDER BY 1,2;
+  
 ALTER TABLE public."HistorySummaryByServiceArea"
   OWNER TO budget_user;
