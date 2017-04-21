1c3bb62a1c22acb252aeadb13c7e648986e75955
Moss Drake <mxmossman@gmail.com>
2017-04-21 16:28:25 -0700

    Update database views to calculate the service area
    I updated the Views that we have so they model the behavior for
    - Elected Officials (EO) service area and for the
    - Management and Finance (MF) burea, which actually falls under LA
    although it's budgeted for many service areas
    
    So, if you query the view, it matches the amounts shown in the Budget in
    Brief
    SELECT fiscal_year, service_area_code, sum
    FROM public."HistorySummaryByServiceArea"
    Where fiscal_year = '2014-15';
    
    1) I added the views through the pgAdmin3 console.
    I originally created the views so I could do some prototyping and to
    better understand the data.
    If we use these views for anything other than that it's probably be
    better to push these views to the db through the Django migration.
    
    2) It seems like PostGres added the "order by" to the views. We don't
    need an "order by" in the views.
    From what I read, PostGres sort of ignores the "order by" in the views.
    But for the most part, "order by" in a view is not good code.
    
    3) We _could_ write Django endpoints that call the views instead of the
    budget history table.
    
    4) Or, we can do it differently: create a calculated column in the
    database that reflects the logic above.
    You can see the logic in the views.

--- sql/View-HistorySummaryByBureau.sql
+++ sql/View-HistorySummaryByBureau.sql
@@ -5,12 +5,16 @@
 
 CREATE OR REPLACE VIEW public."HistorySummaryByBureau" WITH (security_barrier=true) AS 
  SELECT budget_app_budgethistory.fiscal_year,
-    budget_app_budgethistory.service_area_code,
+    case 
+      when budget_app_budgethistory.bureau_code = 'MF' then 'LA'
+      when budget_app_budgethistory.bureau_code in ('MY','PA','PS','PW','PU','AU') then 'EO'
+      else budget_app_budgethistory.service_area_code
+    end as service_area_code,
     budget_app_budgethistory.bureau_code,
     sum(COALESCE(budget_app_budgethistory.amount, 0)) AS sum
    FROM budget_app_budgethistory
-  GROUP BY budget_app_budgethistory.fiscal_year, budget_app_budgethistory.service_area_code, budget_app_budgethistory.bureau_code
-  ORDER BY budget_app_budgethistory.fiscal_year, budget_app_budgethistory.service_area_code, budget_app_budgethistory.bureau_code;
+  GROUP BY 1,2,3
+  ORDER BY 1,2,3;
 
 ALTER TABLE public."HistorySummaryByBureau"
   OWNER TO budget_user;
