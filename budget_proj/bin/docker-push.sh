f758119ed067180a7f779151ceff6cb55d7374d4
Mike Lonergan <mikethecanuck@gmail.com>
2018-05-06 10:37:06 -0700

    testing out the use of the verbose versions of the command-line parameters

--- budget_proj/bin/docker-push.sh
+++ budget_proj/bin/docker-push.sh
@@ -1,9 +1,9 @@
 #! /bin/bash
 
 # Tag, Push and Deploy only if it's not a pull request
-if [ -z "$TRAVIS_PULL_REQUEST" ] || [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
+#if [ -z "$TRAVIS_PULL_REQUEST" ] || [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
   # Push only if we're testing the master branch
-   if [ "$TRAVIS_BRANCH" == "master" ]; then
+#   if [ "$TRAVIS_BRANCH" == "master" ]; then
     export PATH=$PATH:$HOME/.local/bin
     echo Getting the ECR login... # Troubleshooting
     eval $(aws ecr get-login --region $AWS_DEFAULT_REGION)
@@ -11,13 +11,12 @@ if [ -z "$TRAVIS_PULL_REQUEST" ] || [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
     docker push "$DOCKER_REPO"/"$DEPLOY_TARGET"/"$DOCKER_IMAGE":latest
     echo Running ecs-deploy.sh script... # Troubleshooting
     ./$PROJ_SETTINGS_DIR/bin/ecs-deploy.sh  \
-     -n "$ECS_SERVICE_NAME" \
-     -c "$ECS_CLUSTER"   \
-     -i "$DOCKER_REPO"/"$DEPLOY_TARGET"/"$DOCKER_IMAGE":latest \
-     --timeout 300
-   else
-     echo "Skipping deploy because branch is not master"
-  fi
-else
-  echo "Skipping deploy because it's a pull request"
-fi
+        --service-name "$ECS_SERVICE_NAME" \
+        --cluster "$ECS_CLUSTER"   \
+        --image "$DOCKER_REPO"/"$DEPLOY_TARGET"/"$DOCKER_IMAGE":latest \
+        --timeout 300#   else
+#     echo "Skipping deploy because branch is not master"
+#  fi
+#else
+#  echo "Skipping deploy because it's a pull request"
+#fi
