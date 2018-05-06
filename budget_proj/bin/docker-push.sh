e669f75238a1ba9aecea5f0d8dc76785a053521d
Mike Lonergan <mikethecanuck@gmail.com>
2018-05-06 15:31:08 -0700

    ecs-deploy-fixup

--- budget_proj/bin/docker-push.sh
+++ budget_proj/bin/docker-push.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
-# Tag, Push and Deploy only if it's not a pull request
+# Push and Deploy only if it's not a pull request
 #if [ -z "$TRAVIS_PULL_REQUEST" ] || [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
   # Push only if we're testing the master branch
 #   if [ "$TRAVIS_BRANCH" == "master" ]; then
