747bb7537c2926eb8208789ba19228587ed22a63
Mike Lonergan <mikethecanuck@gmail.com>
2019-07-28 19:17:24 -0700

    Maybe this is it - dunno why I have to declare all this environment but let us see if it works and then criticize

--- budget_proj/bin/get-ssm-parameters.sh
+++ budget_proj/bin/get-ssm-parameters.sh
@@ -10,6 +10,10 @@ NAMESPACE="$DOCKER_REPO_NAMESPACE" # future-proofing this script for subsequent 
 PROJECT_CANONICAL_NAME="$PROJECT_NAME" # must be set to each project's "Final naming convention" from here https://github.com/hackoregon/civic-devops/issues/1
 
 # Troubleshooting
+echo $AWS_DEFAULT_REGION
+echo $DOCKER_REPO_NAMESPACE
+echo $PROJECT_NAME
+
 echo $EC2_REGION
 echo $NAMESPACE
 echo $PROJECT_CANONICAL_NAME
