3d8df329a77ccc411382429e498b005eedccc0f8
Mike Lonergan <mikethecanuck@gmail.com>
2019-07-28 18:28:49 -0700

    Looking for where aws cli is

--- budget_proj/bin/test-entrypoint.sh
+++ budget_proj/bin/test-entrypoint.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 export PATH=$PATH:~/.local/bin
+echo What is in local/bin:
+ls ~/.local/bin
 source /code/bin/get-ssm-parameters.sh
 ## TROUBLESHOOTING ONLY
 echo Did get-ssm-parameters.sh pull down the params successfully?
